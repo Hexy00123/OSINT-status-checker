5e6047cf0b5df92c05cecfb060996980981cb715
Vladimir Makharev <v.makharev@yandex.ru>
2024-07-03 20:30:13 +0300

    feat: improvement of tg scrapper via selenium

--- tg_scrapper/src/browser.py
+++ tg_scrapper/src/browser.py
@@ -6,18 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-
-
-API_URL = "http://localhost:8000"
-
-# Constants in seconds
-LOGIN_TIMEOUT = 60
-STATUS_RENDERING_TIMEOUT = 5
-STATUSES_PARSING_DELAY = 60
-
-# Define driver
-driver = webdriver.Chrome()
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
+from selenium.webdriver.support.expected_conditions import staleness_of
 
 
 def login_successful():
@@ -28,19 +18,55 @@ def login_successful():
         return True
 
 
-driver.get(f"https://web.telegram.org/k/")
-
-
 def get_user_status(username):
-    driver.get(f"https://web.telegram.org/k/#@{username}")
-    status_element = WebDriverWait(driver, STATUS_RENDERING_TIMEOUT).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "div.info span.i18n")))
-    status = status_element.text
-    print(status)
-    return "online" in status or 'just now' in status
+    driver.get(TELEGRAM_URL)
+    driver.get(f"{TELEGRAM_URL}#@{username}")
+    # time.sleep(1)
+    status_element = wait.until(
+        EC.visibility_of_element_located(STATUS_LOCATION))
+    print(status_element)
+    for i in range(5):
+        try:
+            status = WebDriverWait(driver, 2).until(
+                EC.visibility_of_element_located(STATUS_LOCATION)).text
+            print(status)
+            break
+        except StaleElementReferenceException:
+            print('exception')
+            pass
+    # try:
+    #     status_element = wait.until(
+    #         EC.presence_of_element_located(STATUS_LOCATION))
+    #     print(status_element)
+    # except TimeoutException:
+    #     return None
+    # try:
+    #     WebDriverWait(driver, 2).until(EC.staleness_of(status_element))
+    # except TimeoutException:
+    #     pass
+    # else:
+    #     status_element = wait.until(
+    #         EC.presence_of_element_located(STATUS_LOCATION))
+    #     # status_element = driver.find_element(*STATUS_LOCATION)
+    print('Full status', status)
+    return "online" in status  # or 'just now' in status
 
 
-response = requests.get(f"{API_URL}/user")
+# Constants in seconds
+STATUS_RENDERING_TIMEOUT = 5
+STATUSES_PARSING_DELAY = 10
+
+# Initialization
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, STATUS_RENDERING_TIMEOUT)
+
+# Other constants
+API_URL = "http://localhost:8000"
+TELEGRAM_URL = "https://web.telegram.org/k/"
+STATUS_LOCATION = (By.CSS_SELECTOR, "div.info span.i18n")
+
+driver.get(TELEGRAM_URL)
+response = requests.get(f"{API_URL}/user/")
 if response.status_code == 200:
     users = response.json()
 else:
@@ -50,20 +76,26 @@ print(users)
 while True:
     if login_successful() and users:
         users_data = []
+        start_time = time.time()
+        status_element = None
         for user in users:
-            user_data = {
-                'ts': str(datetime.now()),
-                'user': {
-                    'id': user.id,
-                    'collection': 'User',
-                },
-                'is_online': get_user_status(user.username),
-            }
-            users_data.append(user_data)
-            print(user_data)
-
-        response = requests.post(f"{API_URL}/status", json=users_data)
-        if response.status_code != 200:
-            print(f"Failed to post user statuses", response)
-
+            user_status = get_user_status(user['username'])
+            if user_status is not None:
+                user_data = {
+                    'ts': str(datetime.now()),
+                    'username': user['username'],
+                    'is_online': user_status,
+                }
+                users_data.append(user_data)
+                print(user_data)
+            else:
+                print('Unable to parse', user)
+        if users_data:
+            response = requests.post(
+                f"{API_URL}/status/create-many", json=users_data)
+            if response.status_code != 200:
+                print(f"Failed to post user statuses", response)
+        else:
+            print('No users data')
+        print(f"For {len(users_data)} it took {time.time() - start_time} seconds")
         time.sleep(STATUSES_PARSING_DELAY)
