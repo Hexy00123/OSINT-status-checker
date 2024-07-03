36bbf914420651fefac27cc2e79e1291c00772fa
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2024-07-03 22:30:48 +0300

    fix: small fix

--- tg_scrapper/src/__main__.py
+++ tg_scrapper/src/__main__.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import uvicorn
 
 os.chdir(Path(__file__).parents[1])
-os.makedirs("files", exist_ok=True)
 
 config = uvicorn.Config(
     "src.main:app", host="0.0.0.0", port=8000, log_level="debug", reload=True
