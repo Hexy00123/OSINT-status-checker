cad7ea496aeed02520f8fbade555a2cce5a629bf
Vladimir Makharev <v.makharev@yandex.ru>
2024-07-05 00:03:28 +0300

    feat: seaborn plot for general distribution

--- frontend/src/statistics_utils.py
+++ frontend/src/statistics_utils.py
@@ -1,13 +1,17 @@
 import datetime
 
 
-def preprocess_data(data):
+def preprocess_data_for_general_distribution(data):
+    # TODO: add time interval params
     statistic = []
 
     for user in data:
-        user['ts'] = user['ts'].split('.')[0]
-        datetime_ts = datetime.datetime.strptime(
-            user['ts'], '%Y-%m-%dT%H:%M:%S')
-        statistic.append(float(f'{datetime_ts.hour}.{datetime_ts.minute}'))
+        # Filter only online timestamps
+        if user['is_online']:
+            # Append each float hour.minute across all users
+            user['ts'] = user['ts'].split('.')[0]
+            datetime_ts = datetime.datetime.strptime(
+                user['ts'], '%Y-%m-%dT%H:%M:%S')
+            statistic.append(float(f'{datetime_ts.hour}.{datetime_ts.minute}'))
 
     return statistic
