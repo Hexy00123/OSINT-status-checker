43061638bb9e29c766550eadb3578a7020592845
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2024-07-05 01:25:22 +0300

    feat: add functionality for user update

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -1,7 +1,10 @@
+import requests
 from fastapi import APIRouter, HTTPException
 from pymongo.errors import DuplicateKeyError
 
 from src.storages.mongo.user import User, UserCreate, user_repository
+
+SCRAPPER_URL = "http://tg_scrapper:8000"
 
 router = APIRouter(prefix="/user", tags=["User"])
 
@@ -10,6 +13,9 @@ router = APIRouter(prefix="/user", tags=["User"])
 async def create(obj: UserCreate) -> User:
     try:
         user = await user_repository.create(obj)
+        response = requests.post(f"{SCRAPPER_URL}/user/update", json={"app": obj.app, "username": obj.username})
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail=f'Failed to update user in tg scrapper: {response.json()}')
     except DuplicateKeyError:
         raise HTTPException(status_code=409)
     return user
@@ -18,12 +24,18 @@ async def create(obj: UserCreate) -> User:
 @router.post("/create-many")
 async def create_many(objs: list[UserCreate]) -> list[str]:
     user_ids = []
+    users_to_update = []
     for obj in objs:
         try:
             user = await user_repository.create(obj)
+            users_to_update.append({"username": obj.username, "app": obj.app})
             user_ids.append(str(user.id))
         except DuplicateKeyError:
             pass
+
+    response = requests.post(f"{SCRAPPER_URL}/user/update-many", json=users_to_update)
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail=f'Failed to update users in tg scrapper: {response.json()}')
     return user_ids
 
 
--- tg_scrapper/src/routers/user.py
+++ tg_scrapper/src/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from src.lifespan import USERS
 from src.models import User
@@ -7,6 +7,20 @@ router = APIRouter(prefix="/user", tags=["User"])
 
 
 @router.post("/update")
-async def create(obj: User):
-    USERS.append(obj)
+async def update(obj: User):
+    for user in USERS:
+        if user["username"] == obj.username:
+            raise HTTPException(status_code=422, detail="Duplicated user")
+    USERS.append({"username": obj.username, "app": obj.app})
+    print(USERS)
+    return
+
+@router.post("/update-many")
+async def update_many(objs: list[User]):
+    for obj in objs:
+        for user in USERS:
+            if user["username"] == obj.username:
+                print(f"Skip user {obj.username} due to duplicate.")
+        USERS.append({"username": obj.username, "app": obj.app})
+    print(USERS)
     return
