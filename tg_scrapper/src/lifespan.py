ae47af17584d32f67bcc314c8996dd11a10fac5d
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2024-07-04 15:55:19 +0300

    fix: fix

--- tg_scrapper/src/lifespan.py
+++ tg_scrapper/src/lifespan.py
@@ -8,7 +8,7 @@ USERS = []
 async def lifespan(app: FastAPI):
     global USERS
 
-    scheduler = BackgroundScheduler(job_defaults={'max_instances': 10})
+    scheduler = BackgroundScheduler()
 
     driver, wait, USERS = scrapper_initialization()
     scheduler.add_job(scrapper_run, "interval", seconds=10, args=[driver, wait, USERS])
